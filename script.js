e28be42ac8f21eef0d34baaf1ba3500588d08f26
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-10-21 12:44:08 -0500

    Added a variable that allows for multiple indents

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+let indent = 0;
+
 onkeydown = function (event) {
     console.log(document.activeElement);
     if (event.key === "Tab") {
         event.preventDefault();
-        document.activeElement.style.textIndent = "1rem";
-    }
-
-    if (event.key === "Backspace") {
-        if (document.activeElement.style.textIndent === "1rem" && document.activeElement.innerHTML === "")
-        document.activeElement.style.textIndent = "0rem";
+        indent++;
+        document.activeElement.style.textIndent = `${indent}rem`;
     }
 }
