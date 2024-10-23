d576c06fd39554dfff876f4154fbe5031bec4ab5
CP-Rishi <161362123+CP-Rishi@users.noreply.github.com>
2024-10-23 12:57:50 -0500

    Changed contenteditable properties for the document

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let indent = 0;
 let shiftDown = false;
 
 document.getElementById("notes").onkeydown = function (event) {
-    console.log(document.activeElement);
     if (event.key === "Tab") {
         if (shiftDown === true) {
             event.preventDefault();
